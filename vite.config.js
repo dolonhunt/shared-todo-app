836ab4d6d9e79aa1c23cd2efa9ecb8f965ce0e3c
dolonhunt <95109852+dolonhunt@users.noreply.github.com>
2025-12-21 18:12:04 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default defineConfig({
-  plugins: [react()],
-  base: './', // ✅ add this line
+  base: './',
 })
